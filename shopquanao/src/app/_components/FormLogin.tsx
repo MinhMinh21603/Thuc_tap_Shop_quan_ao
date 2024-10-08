6c09f95296d91d41986874a06f2584b8f2225c76
Cao Hoàng Thành <160160995+ThanhDevLagi@users.noreply.github.com>
2024-10-09 03:47:17 +0700

    hoàn thành trang đăng nhập 1

---
+++ shopquanao/src/app/_components/FormLogin.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const FormLogin = () => {
+    return (
+        <div className="p-[30px] bg-white shadow-lg rounded-lg flex flex-1">
+            <div className="flex flex-col flex-1 gap-[20px]">
+                <div className='mb-3'>
+                    <h3 className="text-2xl font-semibold text-black mb-[10px]">Login</h3>
+                    <span className="text-base text-gray-500 font-semibold">
+                        Login if you are a returning customer.
+                    </span>
+                </div>
+
+                <div className="flex flex-col gap-[15px]">
+                    <input
+                        type="text"
+                        placeholder="Email Address"
+                        className="p-[12px] border border-gray-300 rounded focus:border-[#ee2761]"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className="p-[12px] border border-gray-300 rounded"
+                    />
+                </div>
+
+                <div className="flex justify-between items-center">
+                    <div className="flex items-center gap-[10px]">
+                        <input type="checkbox" className="form-checkbox w-[18px] h-[18px] accent-[#ee2761]" />
+                        <span className="text-sm text-gray-600 font-semibold">Remember me</span>
+                    </div>
+                    <a href="#" className="text-[#ee2761] text-sm font-semibold">
+                        Forgot Your Password?
+                    </a>
+                </div>
+
+                <button className="bg-[#ee2761] hover:bg-[#061738] transition-colors duration-300 text-white font-semibold py-[12px] rounded">
+                    Login
+                </button>
+
+                <div className="flex items-center justify-between">
+                    <hr className="w-full border-gray-300" />
+                    <span className="mx-[10px] text-sm text-gray-500">OR</span>
+                    <hr className="w-full border-gray-300" />
+                </div>
+
+                <div className="flex justify-between px-3">
+                    <button className="bg-[#4867aa] hover:bg-[#ee2761] transition-all duration-300 text-white py-[10px] w-[30%] rounded font-semibold">
+                        Facebook
+                    </button>
+                    <button className="bg-red-500 hover:bg-[#ee2761] transition-all duration-300 text-white py-[10px] w-[30%] rounded font-semibold">
+                        Google
+                    </button>
+                    <button className="bg-[#55adee] hover:bg-[#ee2761] transition-all duration-300 text-white py-[10px] w-[30%] rounded font-semibold">
+                        Twitter
+                    </button>
+                </div>
+
+                <div className="text-center text-sm text-gray-500 font-semibold">
+                    Don’t have an account? <a href="#" className="text-[#ee2761]">Sign up now</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FormLogin;
---
+++ shopquanao/src/app/_components/FormRegister.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const FormRegister = () => {
+    return (
+        <div className="p-[30px] bg-white shadow-lg rounded-lg flex flex-1">
+            <div className="flex flex-col gap-[20px] flex-1">
+                <div className='mb-3'>
+                    <h3 className="text-2xl font-semibold text-black mb-[10px]">Create an Account</h3>
+                    <span className="text-base text-gray-500 font-semibold">
+                        Register here if you are a new customer.
+                    </span>
+                </div>
+
+                <div className="flex flex-col gap-[15px]">
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        className="p-[12px] border border-gray-300 rounded"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Email Address"
+                        className="p-[12px] border border-gray-300 rounded"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className="p-[12px] border border-gray-300 rounded"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        className="p-[12px] border border-gray-300 rounded"
+                    />
+                </div>
+
+                <button className="bg-[#ee2761] hover:bg-[#061738] transition-colors duration-300 text-white font-semibold py-[12px] rounded">
+                    Submit & Register
+                </button>
+
+                <div className="flex items-center gap-[10px]">    
+                    <input type="checkbox" id="myCheckbox" className="form-checkbox w-[18px] h-[18px] accent-[#ee2761]" />
+                    <span className="text-sm text-gray-600 font-semibold ">
+                        I have read and agree to the terms & conditions
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FormRegister;
